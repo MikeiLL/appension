afea059496e42ebaac50fc7bbbe8e58488df5190
Mike iLL Kilmer <mike@mzoo.org>
2014-10-31 17:31:33 -0500

    extrack track details from mp3.id3 using mutagen.mp3

---
+++ id3info.py
@@ -0,0 +1,30 @@
+'''
+Extract MP3 metadata from MP3 file ID3 tags
+
+'''
+
+usage = '''
+python id3info.py
+'''
+from mutagen.mp3 import MP3
+import glob
+
+def titles(files):
+	print(len(files))
+	for file in files:
+		track = MP3(file)
+		try:
+			print("Track info is: {} - {} - {}".format(
+				track['TPE1'].text, 
+				track['TIT2'].text,
+				track['TALB'].text))
+		except KeyError:
+			pass
+		
+if __name__ == "__main__":
+	print("ID3 details:")
+	files = glob.glob('audio/*.mp3')
+	try:
+		titles(files)
+	except IOError:
+		print usage
