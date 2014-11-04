19f485610c44a3723b3bda0a0aec4ded793c3762
Chris Angelico <rosuav@gmail.com>
2014-11-04 11:27:06 +1100

    Give Track objects a magic .obj attribute

--- fore/database.py
+++ fore/database.py
@@ -8,6 +8,7 @@ class Track(object):
 	def __init__(self, id, filename):
 		self.id = id
 		self.filename = filename
+		self.obj = utils.Magic_Anything("Track_"+id)
 
 def get_mp3(some_specifier):
 	with _conn.cursor():
