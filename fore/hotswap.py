1a211c264de52fbd4719aaa130129f73388a5dd4
Chris Angelico <rosuav@gmail.com>
2014-11-04 14:57:15 +1100

    Hotswap: Remove all references to actually swapping

--- fore/hotswap.py
+++ fore/hotswap.py
@@ -8,26 +8,13 @@ log = logging.getLogger(__name__)
 class Hotswap(threading.Thread):
     def __init__(self, out, mod, *args, **kwargs):
         self.out = out
-        self.mod = mod
         self.gen = mod.generate(*args, **kwargs)
-        self.loaded = self.current_modtime
-        self.args = args
-        self.kwargs = kwargs
 
         threading.Thread.__init__(self)
         self.daemon = True
 
-    @property
-    def current_modtime(self):
-        return os.path.getmtime(self.mod.__file__.replace("pyc", "py"))
-
     def run(self):
         while True:
-            if self.current_modtime != self.loaded:
-                log.info("Hot-swapping module: %s", self.mod.__name__)
-                # self.mod = reload(self.mod)
-                self.loaded = self.current_modtime
-                self.gen = self.mod.generate(*self.args, **self.kwargs)
             self.handle(self.gen.next())
 
     def handle(self, elem):
