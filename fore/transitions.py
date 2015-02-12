c65b94c3174607e5d534d479c5d7594ce6eb7d09
Chris Angelico <rosuav@gmail.com>
2015-02-12 16:40:34 +1100

    Remove the 1s glitch - cross-fading should now work correctly!

--- fore/transitions.py
+++ fore/transitions.py
@@ -76,7 +76,7 @@ def managed_transition(track1, track2):
         mix_duration = t1end - playback_end
         
         pb1 = pb(track1, start_point['cursor'], playback_duration)
-        pb2 = cf((track1, track2), (playback_end - .01, 1), mix_duration - 1)
+        pb2 = cf((track1, track2), (playback_end - .01, t2start), mix_duration)
         log.warning("Mix looks like start %r track: %r end: %r, duration: %r",pb2.s2, pb2.t2, pb2.e2, pb2.duration)
 
         log.warning("""
@@ -98,7 +98,7 @@ def managed_transition(track1, track2):
              playback_end, t1end, mix_duration,
              equal, actual, desired, diff, t2start)
         log.warning("Actual mix duration is %r.",pb2.duration)
-        start_point['cursor'] = mix_duration - 1 + t2start
+        start_point['cursor'] = mix_duration + t2start
         return [pb1, pb2]
 
 def lead_in(track):
