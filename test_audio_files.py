11b28b70c1d8770e25411b4cae915b247bcdfd42
Mike iLL Kilmer <mike@mzoo.org>
2015-03-12 18:15:10 -0500

    Do it for all the disabled tracks shorter than 700 seconds.

--- test_audio_files.py
+++ test_audio_files.py
@@ -3,8 +3,12 @@ import fore.mixer
 import fore.database
 import pyechonest.track
 
-for file in fore.database.get_many_mp3(status='all'):
-	print("Name: {} Length: {}".format(file.filename, file.track_details['length']))
-	track = track.track_from_filename('audio/'+file.filename, force_upload=True)
-	print(track.id)
+for file in fore.database.get_many_mp3(status=0):
+	if file.track_details['length'] < 700:
+		print("Name: {} Length: {}".format(file.filename, file.track_details['length']))
+		track = track.track_from_filename('audio/'+file.filename, force_upload=True)
+		print(track.id)
+	else:
+		print("BIG ONE - Name: {} Length: {}".format(file.filename, file.track_details['length']))
+		
 
