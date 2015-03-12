7a8bb6e43c61a3085efe2a6d56428c79eb9329ad
Chris Angelico <rosuav@gmail.com>
2015-03-12 21:10:15 +1100

    Replace the YAML configs with simple Python modules

--- fore/config.py
+++ fore/config.py
@@ -1,12 +1,27 @@
-import os
-import sys
-from liveyamlfile import LiveYamlFile
+# Basic config for stuff that can be easily changed, but which is git-managed.
+# See also apikeys_sample.py for the configs which are _not_ git-managed.
+app_name = "fore"
+server_domain = "http://www.infiniteglitch.net"
 
+log_file = 'log.txt'
+log_format = '%(asctime)s P%(process)-5d (%(levelname)8s) %(module)16s%(lineno)5d: %(uid)9s %(message)s'
+log_config_file_changes = False
 
-class ConfigFile(LiveYamlFile):
-	pass
+lag_limit = 88200        #   samples - how much we can lag by before dropping frames.
+restart_timeout = 3      #   seconds between polls to restart.txt
+http_port = 80
+mini_http_port = 8193
+uid = 1000 # User ID and group ID to drop privileges to
+gid = 1000 # Set both to 0 to not drop privileges, eg if the server is started without privs
+use_sudo_uid_gid = True # If set, uid/gid will be overridden with SUDO_UID/SUDO_GID if available
+frontend_buffer = 20    #   seconds of audio to buffer in frontend
+past_played_buffer = 600 #   seconds of audio to store track metadata for in the past
+monitor_update_time = 0.5
+template_dir = "templates/"
+drift_limit = 0.1        #   seconds of audio after which drift should be corrected
 
-# This is a dirty, dirty hack, but lets you just do:
-#   import config
-# and have access to an instantiated config object.
-sys.modules[__name__] = ConfigFile(os.path.join(*(os.path.dirname(__file__).split(os.sep)[:-1] + ['config.yml'])))
+max_track_length = 400
+min_track_length = 90
+
+#   Default values when nothing exists
+no_bpm_diff = 20
