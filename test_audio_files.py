11885d88cf3962eee5ea74eef1c90bd2a6b496dd
Mike iLL Kilmer <mike@mzoo.org>
2015-03-12 08:40:35 -0500

    Can we change echonest timeout?

--- test_audio_files.py
+++ test_audio_files.py
@@ -1,8 +1,10 @@
 import fore.apikeys
 import fore.mixer
 import fore.database
+import pyechonest.track
+pyechonest.track.track_from_md5.func_defaults=120,
 
-for file in fore.database.get_many_mp3():
-	print(file.filename)
+for file in fore.database.get_many_mp3(status='all'):
+	print("Name: {} Length: {}".format(file.filename, file.track_details.length))
 	stream = fore.mixer.LocalAudioStream('audio/'+file.filename)
 
