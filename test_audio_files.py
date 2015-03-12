cea891a2de493ce211ccf13f4bf0c487f945985d
Mike iLL Kilmer <mike@mzoo.org>
2015-03-12 18:09:47 -0500

    Print out the new track id.

--- test_audio_files.py
+++ test_audio_files.py
@@ -5,5 +5,6 @@ import pyechonest.track
 
 for file in fore.database.get_many_mp3(status='all'):
 	print("Name: {} Length: {}".format(file.filename, file.track_details['length']))
-	track.track_from_filename('audio/'+file.filename, force_upload=True)
+	track = track.track_from_filename('audio/'+file.filename, force_upload=True)
+	print(track.id)
 
