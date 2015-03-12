96df328a39582e53a657a0d48e357cfb178257d6
Mike iLL Kilmer <mike@mzoo.org>
2015-03-12 18:04:17 -0500

    Force upload all the tracks.

--- test_audio_files.py
+++ test_audio_files.py
@@ -2,9 +2,8 @@ import fore.apikeys
 import fore.mixer
 import fore.database
 import pyechonest.track
-pyechonest.track.track_from_md5.func_defaults=120,
 
 for file in fore.database.get_many_mp3(status='all'):
 	print("Name: {} Length: {}".format(file.filename, file.track_details['length']))
-	stream = fore.mixer.LocalAudioStream('audio/'+file.filename)
+	track.track_from_filename('audio/'+file.filename, force_upload=True)
 
