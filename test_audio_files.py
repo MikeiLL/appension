3de7c51621436ce2432077a3e55cad9965649a1e
Mike iLL Kilmer <mike@mzoo.org>
2015-03-12 08:42:46 -0500

    Get 'length' in a way that will work.

--- test_audio_files.py
+++ test_audio_files.py
@@ -5,6 +5,6 @@ import pyechonest.track
 pyechonest.track.track_from_md5.func_defaults=120,
 
 for file in fore.database.get_many_mp3(status='all'):
-	print("Name: {} Length: {}".format(file.filename, file.track_details.length))
+	print("Name: {} Length: {}".format(file.filename, file.track_details['length']))
 	stream = fore.mixer.LocalAudioStream('audio/'+file.filename)
 
