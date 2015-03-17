0bd47a20f2e130f8046f6accc709faabaf35de08
Mike iLL Kilmer <mike@mzoo.org>
2015-03-17 11:22:13 -0500

    Add missing <div id="fb-root"></div>.

--- assets/recorder.js
+++ assets/recorder.js
@@ -168,48 +168,24 @@
 
   };
   
-function postRefreshPage () {
-  var theForm, newInput1, newInput2;
-  // Start by creating a <form>
-  theForm = document.createElement('form');
-  theForm.action = 'somepage.php';
-  theForm.method = 'post';
-  // Next create the <input>s in the form and give them names and values
-  newInput1 = document.createElement('input');
-  newInput1.type = 'hidden';
-  newInput1.name = 'input_1';
-  newInput1.value = 'value 1';
-  newInput2 = document.createElement('input');
-  newInput2.type = 'hidden';
-  newInput2.name = 'input_2';
-  newInput2.value = 'value 2';
-  // Now put everything together...
-  theForm.appendChild(newInput1);
-  theForm.appendChild(newInput2);
-  // ...and it to the DOM...
-  document.getElementById('hidden_form_container').appendChild(theForm);
-  // ...and submit it
-  theForm.submit();
-}
-    
 	function uploadAudio(mp3Data){
-		var newInput1, newInput2;
-		var fd = new FormData();
-        document.getElementById('upload_button').style.display = "inline";
-		var mp3Name = encodeURIComponent('audio_recording_' + new Date().getTime() + '.mp3');
-		console.log("mp3name = " + mp3Name);
-		fd.append('fname', mp3Name);
-		fd.append('data', mp3Data);
 		var reader = new FileReader();
-		// Start by creating a <form>
-		theForm = document.createElement('form');
-		fd.action = 'recorder';
-		fd.method = 'post';
-		// ...and it to the DOM...
-		// ...and submit it
-		fd.submit();
-
-		console.log('UPLOADED to SERVER');
+		reader.onload = function(event){
+			var fd = new FormData();
+			var mp3Name = encodeURIComponent('audio_recording_' + new Date().getTime() + '.mp3');
+			console.log("mp3name = " + mp3Name);
+			fd.append('fname', mp3Name);
+			fd.append('data', event.target.result);
+			$.ajax({
+				type: 'POST',
+				url: 'recorder',
+				data: fd,
+				processData: false,
+				contentType: false
+			}).done(function(data) {
+				console.log('Upload.php');
+			});
+		};      
 		reader.readAsDataURL(mp3Data);
 	}
 	
