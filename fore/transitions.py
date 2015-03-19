7e9e2b2fb88523efb4cb7dbe9175db7745f08be7
Mike iLL Kilmer <mike@mzoo.org>
2015-03-19 18:08:23 -0500

    Remove a log entry.

--- fore/transitions.py
+++ fore/transitions.py
@@ -77,8 +77,6 @@ def managed_transition(track1, track2, state=None):
         pb2 = pb(track2, t2start, t2_length - t2_otrim - 2)
         '''Set cursor to 2 seconds'''
         state['cursor'] = max(t2start + t2_length - t2_otrim - 2, 0)
-        log.warning("""No xfade and %r, plus
-        %r""",str(pb1), str(pb2))
         return [pb1, pb2]
     else:
         '''offset between start and first theoretical beat.'''
