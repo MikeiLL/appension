f69a41726abdf7a79073bf1e3217e6c4b8aef34f
Mike iLL Kilmer <mike@mzoo.org>
2015-03-24 10:07:05 -0500

    Import LocalAudioStream.

--- manipulate.py
+++ manipulate.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python -i
 import fore.action as action
 from echonest.remix.audio import LocalAudioFile
-print("You can load MP3 files with LocalAudioFile()")
+from fore.mixer import LocalAudioStream as las
+print("You can load MP3 files with LocalAudioFile() or las()")
 print("and manipulate them using the functions in action")
 print("To save a modified file: save(f, fn)")
 def save(f, fn):
     action.audition_render([f.data], fn)
+    
+
