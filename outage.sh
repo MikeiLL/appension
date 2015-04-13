18a5c7a957d1d8482aa9826acde44781af90def0
Mike iLL Kilmer <mike@mzoo.org>
2015-04-13 18:52:57 -0500

    Set max time to 10 seconds.

--- outage.sh
+++ outage.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Run with Sudo
 NOTIFYEMAIL=mike@mzoo.org
 SMSEMAIL=2016794168@pm.sprint.com
 SENDEREMAIL=mikekilmer@infiniteglitch.net
@@ -10,7 +9,7 @@ DEBUG=0
 
 while true 
 do
-/usr/bin/curl -sSf $SERVER > /dev/null 2>&1
+/usr/bin/curl -sSf --max-time 10 $SERVER > /dev/null 2>&1
 CS=$?
 # For debugging purposes
 if [ $DEBUG -eq 1 ]
@@ -30,29 +29,34 @@ fi
 # If the server is down and no alert is sent - alert
 if [ $CS -ne 0 ] && [ $FAILED -eq 0 ]
 then
-    FAILED=1
     PID=`ps -eaf | grep 'python -m fore.server' | grep -v grep | awk '{print $2}'`
+    FAILED=1
     if [ $DEBUG -eq 1 ]
     then
-    	if [[ "" !=  "$PID" ]]
+        echo "$SERVER failed"
+        if [[ "" !=  "$PID" ]]
         then
-        	echo "$SERVER failed. Killing Process..."
+        	echo "killing $PID"
         	kill -9 $PID
-        	echo "Starting the server..."
+        	echo "Starting the server."
         	python -m fore.server
         else
-        	echo "No Such Process Running Locally."
+        	echo "No Python Process Running Locally"
         fi
     fi
     if [ $DEBUG = 0 ]
     then
-        echo "$SERVER went down $(date)" | /usr/bin/mail -s "$SERVER went down" "$SENDEREMAIL" "$SMSEMAIL" " < error.log" 
-        echo "$SERVER went down $(date)" | /usr/bin/mail -s "$SERVER went down" "$SENDEREMAIL" "$NOTIFYEMAIL" " < error.log"
+        echo "$SERVER failed"
+        PID=`ps -eaf | grep 'python -m fore.server' | grep -v grep | awk '{print $2}'`
         if [[ "" !=  "$PID" ]]
         then
         	kill -9 $PID
+        	echo "$SERVER went down $(date)" | /usr/bin/mail -s "$SERVER went down" "$SENDEREMAIL" "$SMSEMAIL" 
+        	echo "$SERVER went down $(date)" | /usr/bin/mail -s "$SERVER went down" "$SENDEREMAIL" "$NOTIFYEMAIL"
         	python -m fore.server
-        fi
+        else
+        	echo "No Python Process Running Locally"
+        fi 
     fi
 
 # If the server is back up and no alert is sent - alert
@@ -65,8 +69,8 @@ then
     fi
     if [ $DEBUG = 0 ]
     then
-        echo "$SERVER is back up $(date)" | /usr/bin/mail -s "$SERVER is back up again" "$SENDEREMAIL" "$SMSEMAIL" " < error.log"
-        echo "$SERVER is back up $(date)" | /usr/bin/mail -s "$SERVER is back up again" "$SENDEREMAIL" "$NOTIFYEMAIL" " < error.log"
+        echo "$SERVER is back up $(date)" | /usr/bin/mail -s "$SERVER is back up again" "$SENDEREMAIL" "$SMSEMAIL"
+        echo "$SERVER is back up $(date)" | /usr/bin/mail -s "$SERVER is back up again" "$SENDEREMAIL" "$NOTIFYEMAIL"
     fi
 fi
 sleep $PAUSE
