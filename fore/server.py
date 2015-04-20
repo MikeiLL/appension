c8fa22cf757dddb6b2556cb8027bc747887c5027
Chris Angelico <rosuav@gmail.com>
2015-04-21 02:29:09 +1000

    Clean up update_track handling of artwork

--- fore/server.py
+++ fore/server.py
@@ -966,8 +966,8 @@ class ConfirmTransition(BaseHandler):
 		next_track_id = self.request.arguments.pop('next_track_id')[0]
 		in_track_data['itrim'] = self.request.arguments.pop('itrim')
 		out_track_id = self.request.arguments.pop('track_id')[0]
-		database.update_track(out_track_id, self.request.arguments, artwork=None)
-		database.update_track(next_track_id, in_track_data, artwork=None)
+		database.update_track(out_track_id, self.request.arguments)
+		database.update_track(next_track_id, in_track_data)
 		self.write(admin_page(user_name, notice="Transition Settings Adjusted"))
 				
 @route("/sb")
