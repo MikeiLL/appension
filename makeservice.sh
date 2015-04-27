081bb77b689e629b95b4542ab0977a6b8e6cd953
Chris Angelico <rosuav@gmail.com>
2015-04-28 01:09:24 +1000

    Oops - cwd is descriptive, pwd is the command

--- makeservice.sh
+++ makeservice.sh
@@ -3,7 +3,7 @@ Description=Infinite Glitch
 
 [Service]
 ExecStart=`which python` -m fore.server
-WorkingDirectory=`cwd`
+WorkingDirectory=`pwd`
 "|sudo tee /etc/systemd/system/glitch.service >/dev/null
 echo "Service file created. To start:"
 echo sudo systemctl start glitch.service
