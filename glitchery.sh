b40964a5c5adf0261349fb599333e4a5a0d6d5da
Mike iLL <mike@mzoo.org>
2015-06-04 03:58:10 +0000

    Keith updates.

--- glitchery.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-echo "Starting InfiniteGlitch."
-until python -m fore.server 2>&1 | tee error.log; do
-echo "Server 'python -m fore.server 2>&1 | tee error.log' crashed with exit code $?. Respawning.." >&2
-sleep 1
-mail -s "server crashed and restarted" mike@madhappy.com < error.log
-done
-
