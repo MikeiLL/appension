c2dbfc7f18dc44747fbb8b14e212cbb4151e8f85
Mike iLL Kilmer <mike@mzoo.org>
2016-12-08 19:59:54 -0600

    Send track number as CLI argument.

--- analyze.py
+++ analyze.py
@@ -1,5 +1,11 @@
+import sys
 import fore.database
-analysis = fore.database.get_analysis(2)
+
+if len(sys.argv) > 1:
+    track_no = sys.argv[1]
+else:
+    track_no = 2
+analysis = fore.database.get_analysis(track_no)
 import pickle, base64
 analysis = pickle.loads(base64.b64decode(analysis))
 print(analysis)
