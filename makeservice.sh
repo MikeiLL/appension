581d91cf9540dfbb495cfe6902ecb601e2a9869e
Chris Angelico <rosuav@gmail.com>
2016-12-23 06:07:42 +1100

    Update makeservice to use the new package (closes #203)

--- makeservice.sh
+++ makeservice.sh
@@ -2,8 +2,15 @@ echo "[Unit]
 Description=Infinite Glitch
 
 [Service]
-ExecStart=`which python` -m fore.server
+ExecStart=`which python` -m glitch
 WorkingDirectory=`pwd`
 "|sudo tee /etc/systemd/system/glitch.service >/dev/null
+echo "[Unit]
+Description=Infinite Glitch Renderer
+
+[Service]
+ExecStart=`which python` -m glitch renderer
+WorkingDirectory=`pwd`
+"|sudo tee /etc/systemd/system/glitch-renderer.service >/dev/null
 echo "Service file created. To start:"
-echo sudo systemctl start glitch.service
+echo sudo systemctl start glitch glitch-renderer
