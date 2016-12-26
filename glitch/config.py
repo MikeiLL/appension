ae55c6fd7c8a1af2598fde015b4e45ef7795a602
Mike Kilmer <mikekilmer@debian>
2016-12-26 13:33:16 -0500

    We probably don't want config parameters, or at least not all of them, to be git tracked, do we?

--- glitch/config.py
+++ glitch/config.py
@@ -1,6 +1,7 @@
 # Basic config for stuff that can be easily changed, but which is git-managed.
 # See also apikeys_sample.py for the configs which are _not_ git-managed.
-server_domain = "http://www.infiniteglitch.net"
+#server_domain = "http://www.infiniteglitch.net"
+server_domain = "http://50.116.55.59"
 
 http_port = 8888 # Port for the main web site
 renderer_port = 8889 # Port for the renderer (/all.mp3 and friends)
