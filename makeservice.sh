090daca5f7bec9f2057e02be3e260868946fa261
Chris Angelico <rosuav@gmail.com>
2016-12-28 01:10:16 +1100

    Have systemd provide a socket to Glitch

--- makeservice.sh
+++ makeservice.sh
@@ -1,11 +1,15 @@
 echo "[Unit]
 Description=Infinite Glitch
+Requires=glitch.socket
 
 [Service]
 User=`whoami`
 ExecStart=`which python` -m glitch
 WorkingDirectory=`pwd`
 "|sudo tee /etc/systemd/system/glitch.service >/dev/null
+echo "[Socket]
+ListenStream=80
+"|sudo tee /etc/systemd/system/glitch.socket >/dev/null
 echo "[Unit]
 Description=Infinite Glitch Renderer
 
@@ -14,5 +18,6 @@ User=`whoami`
 ExecStart=`which python` -m glitch renderer
 WorkingDirectory=`pwd`
 "|sudo tee /etc/systemd/system/glitch-renderer.service >/dev/null
+sudo systemctl daemon-reload
 echo "Service file created. To start:"
 echo sudo systemctl start glitch glitch-renderer
