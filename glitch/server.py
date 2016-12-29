18a85e6beced58f33ce46d0a3e8d65a27ef6d48d
Chris Angelico <rosuav@gmail.com>
2016-12-30 05:24:35 +1100

    Parameterize the status.json URI too

--- glitch/server.py
+++ glitch/server.py
@@ -71,7 +71,7 @@ def home():
 	complete_length = datetime.timedelta(seconds=int(database.get_complete_length()))
 	return render_template("index.html",
 		open=True, # Can have this check for server load if we ever care
-		endpoint="http://%s:%d/all.mp3" % (urlparse(request.url_root).netloc.split(":")[0], renderer_port),
+		renderer="http://%s:%d/" % (urlparse(request.url_root).netloc.split(":")[0], renderer_port),
 		complete_length=complete_length,
 		couplet_count=couplet_count(lyrics),
 		lyrics=lyrics,
