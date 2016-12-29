5037a0497aa78d33acb57e3df405292f6b0d1958
Chris Angelico <rosuav@gmail.com>
2016-12-29 23:51:16 +1100

    Remove all the code that predates the MikeILL/Rosuav era.
    None of this was being used anywhere anyway.

--- glitch/utils.py
+++ glitch/utils.py
@@ -1,34 +1,9 @@
-#!/usr/bin/env python
-from __future__ import print_function
-"""
-utils.py
-
-Created by Jason Sundram, on 2010-04-05.
-Copyright (c) 2010 The Echo Nest. All rights reserved.
-Expanded Chris Angelico 2014 with additional utilities.
-"""
-import threading, os
+import os
 import binascii
 import hashlib
 import collections
 import random
 import time
-
-def flatten(l):
-	""" Converts a list of tuples to a flat list.
-		e.g. flatten([(1,2), (3,4)]) => [1,2,3,4]
-	"""
-	return [item for pair in l for item in pair]
-
-def tuples(l, n=2):
-	""" returns n-tuples from l.
-		e.g. tuples(range(4), n=2) -> [(0, 1), (1, 2), (2, 3)]
-	"""
-	return zip(*[l[i:] for i in range(n)])
-
-def rows(m):
-	"""returns the # of rows in a numpy matrix"""
-	return m.shape[0]
 
 magic_log = None
 
@@ -60,23 +35,6 @@ class Magic_Anything(object):
 		print(repr(self) + "." + name, file=magic_log); magic_log.flush()
 		return Magic_Str(repr(self) + "." + name)
 
-def shuffler(func, gen):
-	"""Call func(next(gen)) repeatedly.
-
-	TODO: Should this become for x in gen: func(x) ?
-	Currently, a StopIteration will bubble unexpectedly.
-
-	Not currently used.
-	"""
-	while True:
-		func(next(gen))
-
-def daemonize(target, *args):
-	"""Start a daemon thread to call target(*args)."""
-	t = threading.Thread(target=target, args=args)
-	t.daemon = True
-	t.start()
-	
 def random_hex():
 	return binascii.b2a_hex(os.urandom(8)).decode("ascii")
 
