b6fea2a3cd23c9f24935a2a3b78636ac036c7665
Chris Angelico <rosuav@gmail.com>
2019-07-31 01:47:37 +1000

    Oops - pop in the omitted percent marker

--- glitch/server.py
+++ glitch/server.py
@@ -72,7 +72,7 @@ def home():
 	prot = "https" if os.stat("privkey.pem") else "http" # TODO: See if the incoming request was over HTTPS instead
 	return render_template("index.html",
 		open=True, # Can have this check for server load if we ever care
-		renderer="://%s:%d/" % (prot, urlparse(request.url_root).netloc.split(":")[0], renderer_port),
+		renderer="%s://%s:%d/" % (prot, urlparse(request.url_root).netloc.split(":")[0], renderer_port),
 		complete_length=complete_length,
 		couplet_count=couplet_count(lyrics),
 		lyrics=lyrics,
