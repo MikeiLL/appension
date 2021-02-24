4c9044ba9d54a0f983a09d45b9e810c7f162190f
Mike iLL Kilmer <mike@mzoo.org>
2021-02-23 20:40:18 -0600

    Print some test data for admin login debugging.

--- glitch/server.py
+++ glitch/server.py
@@ -170,9 +170,14 @@ def login_get():
 
 @app.route("/login", methods=["POST"])
 def login_post():
+	print("in login post")
 	user = database.User.from_credentials(request.form["email"], request.form["password"])
+	print(user)
 	if user: login_user(user)
 	url = request.args.get("next") or "/"
+	print(url)
+	url_test = is_safe_url(url)
+	print("url test result: %r" % (url_test))
 	if not is_safe_url(url): url = "/"
 	return redirect(url)
 
