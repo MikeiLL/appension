fda46b2dad2d2dd59567960c3a525960023e04f7
Mike iLL Kilmer <mike@mzoo.org>
2021-03-01 20:15:12 -0600

    Back to no db request on admin page.

--- glitch/server.py
+++ glitch/server.py
@@ -356,7 +356,6 @@ def oracle_get():
 @admin_required
 def admin():
 	# all_tracks = database.get_many_mp3("all", "sequence, id")
-	all_tracks = database.get_complete_length()
 	return render_template("mini.html")
 	return render_template("administration.html", all_tracks=all_tracks)
 	
